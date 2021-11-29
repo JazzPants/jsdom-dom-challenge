ac1c5c7183ddf428e1a579402a42dac4742135ec
JazzPants <leighton.l14@gmail.com>
2021-11-29 15:08:45 +1100

    Second Draft

--- js/challenge.js
+++ js/challenge.js
@@ -1,12 +1,23 @@
-// document.addEventListener("DOMContentLoaded", ())
+// document.addEventListener("DOMContentLoaded", setInterval)
 
+let interval = setInterval(increment, 1000);
+
+function increment() {
+    let x = parseInt(counterBox.innerText, 10);
+    x++;
+    return counterBox.innerHTML = x;
+}
 
 
 const counterBox = document.getElementById('counter');
 const plusButton = document.getElementById('plus');
 const minusButton = document.getElementById('minus');
 const likeButton = document.getElementById('heart');
+const pauseButton = document.getElementById('pause');
+const submitButton = document.getElementById('submit');
+
 const likeList = document.querySelector('.likes');
+let clicks = 0;
 
 function upNumber() {
     console.log('plus button clicked!');
@@ -20,7 +31,7 @@ function downNumber() {
     console.log('minus button clicked!');
     console.log(counterBox.innerText);
     let x = parseInt(counterBox.innerText, 10);
-    value = isNaN(value) ? 0 : value; //if value is NaN change to 0, otherwise keep 'value'
+    // value = isNaN(value) ? 0 : value; //if value is NaN change to 0, otherwise keep 'value'
     if (x > 0) {
         x--;
     }
@@ -29,18 +40,52 @@ function downNumber() {
 
 function addLike() {
     console.log('like button clicked!');
-    let x = counterBox.innerText;
-    let clicks = 0; 
-    console.log(`clicked ${clicks} times!`)
-    li = document.createElement("li")
-    li.innerHTML = `${x} was liked ${clicks++} times!`
-    likeList.appendChild(li);
+    let x = parseInt(counterBox.innerText, 10);
+    let currentNumber = x;
+    console.log(`${x}`);
+    console.log(`${currentNumber}`);
+    // if (currentNumber === x) {
+    //     clicks++;
+    // } else {
+    //     clicks = 0
+    // }
+    
 
+    li = document.createElement("li");
+    li.innerHTML = `${x} was liked ${clicks} times!`;
+    likeList.appendChild(li); 
+    if (currentNumber === x) {
+        
+    } else {
+        
+    }
+    
+    
+}
+
+function pauseCounter() {
+    const x = pauseButton.innerText;
+    if (x === "pause") {
+        pauseButton.innerHTML = "resume"
+        clearInterval(interval)
+        interval = null;
+    } else {
+        pauseButton.innerHTML = "pause"
+        interval = setInterval(increment, 1000)
+    }
+}
+
+function submitContent(event) {
+    event.preventDefault();
 }
 
 plusButton.addEventListener('click', upNumber);
 minusButton.addEventListener('click', downNumber);
-likeButton.addEventListener('click', addLike)
+likeButton.addEventListener('click', addLike);
+pauseButton.addEventListener('click', pauseCounter);
+submitButton.addEventListener('click', submitContent)
+
+
 // let intervalID;
 // function changeNumber() {
 // if (!intervalID) {
